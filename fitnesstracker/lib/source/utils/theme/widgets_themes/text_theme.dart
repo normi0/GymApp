e1ecd8f08089489e3dcae98b95df48bd3b72db49
Achi seif <seif23achi@gmail.com>
2023-11-04 06:26:34 +0100

    adding a splash screen

--- fitnesstracker/lib/source/utils/theme/widgets_themes/text_theme.dart
+++ fitnesstracker/lib/source/utils/theme/widgets_themes/text_theme.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class ClassName {
+class TtextTheme {
   static TextTheme lightTextTheme = TextTheme(
     headlineSmall: GoogleFonts.montserrat(
       color: Colors.black87,
